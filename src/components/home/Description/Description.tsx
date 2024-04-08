8098c887b2f97b52eb3d886bedf421bbb2ed1660
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-04-08 12:48:15 -0300

    next Image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,11 +1,22 @@
 import styles from './Description.module.sass';
+import Image from 'next/image';
+
 
 export const Description = () => {
   return (
-    <section>
-      <img src="/images/description.jpeg" alt="Products marketplace" />
-      <h2>Bring the future today</h2>
-      <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+    <section className={styles.Description}>
+      <Image 
+        src="/images/description.jpeg" 
+        alt="Products marketplace" 
+        width={500}
+        height={300}
+        priority={true}
+        quality={100}
+      />
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+      </div>
     </section>
   )
 }
